ea92424ea87179e4ca33719722e95953bae51e5f
Miguel Ortega Ward <miguel.owd@gmail.com>
2022-03-10 09:32:29 +0100

    cambios miguel

--- src/components/News/NewsCard/NewsCard.jsx
+++ src/components/News/NewsCard/NewsCard.jsx
@@ -4,7 +4,7 @@ import './NewsCard.css'
 
 const NewsCard = ({ _id, title, description, imgURL }) => {
     return (
-        <Container >
+        <Container className="news-card">
             <Col>
                 <div class="a-box ">
                     <div class="img-container ">
--- src/pages/IndexPage/IndexPage.jsx
+++ src/pages/IndexPage/IndexPage.jsx
@@ -1,11 +1,10 @@
 import UserMessage from "../../components/UserMessage/UserMessage"
-import { Container, Button, Row, Col, Image, Card } from 'react-bootstrap'
+import { Container, Button, Row, Col, Image, Card, NavLink } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import './IndexPage.css'
 import NewsCard from "../../components/News/NewsCard/NewsCard"
 import Facebook from "../../components/Facebook/Facebook"
 import Twitter from "../../components/Twitter/Twitter"
-import { Link } from "react-router-dom"
 
 import news1 from './img/imgNoticia1.jpeg'
 import news2 from './img/imgNoticia2.jpeg'
@@ -20,154 +19,161 @@ const IndexPage = () => {
 
     return (
         <>
-            <UserMessage />
-            <div className="mb">
-                <section className="hero parallax">
-                    <Row >
-                        <Col>
-                            <br /><br />
-                            <h1>Club de Vuelo UPM Akaflieg Madrid</h1>
-                            <p>La primera asociación universitaria en España dedicada a la ingeniería del vuelo a vela</p>
-                            <button className="btn-effect center"><span>Sobre Nosotros!</span></button>
+            <div className="index-page">
+                <UserMessage />
 
-                        </Col>
-                    </Row>
+                <div className="mb">
+                    <section className="hero parallax">
+                        <Row >
+                            <Col>
+                                <br /><br />
+                                <h1 className="center">Club de Vuelo UPM</h1>
+                                <h2 className="center">Akaflieg Madrid</h2>
+                                <p>La primera asociación universitaria en España dedicada a la ingeniería del vuelo a vela</p>
+                                <button className="btn-effect center conocenos"> <Link style={{ color: "white" }} to={`/about-us`}>Conócenos</Link></button>
 
-                </section>
-            </div>
 
-            <Container >
-                <section className="newsSection">
-                    <Row>
-                        <Col className="newsTitle">Noticias</Col>
-                    </Row>
-                    <Row>
-                        <Col >
-                            <Card className="newsCard">
-                                <Card.Img src={news1} />
-                                <Card.Body >
-                                    <Card.Title className="center blue">Sommertreffen 2019</Card.Title>
-                                    <Card.Text >
-                                        12 Sep , 2019
-                                        <br />
-                                        Desde el Club de Vuelo, entre muchas otras cosas, buscamos fomentar la cultura aeronáutica a través nuestra participación y organización de actividades</Card.Text>
-                                    <Link to={`/news/`}>
-                                        <Button>Leer más</Button>
-                                    </Link>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col >
-                            <Card className="newsCard">
-                                <Card.Img src={news2} />
-                                <Card.Body className="">
-                                    <Card.Title className="center blue">Cursillo ATC IVAO</Card.Title>
-                                    <Card.Text>
-                                        16 Mar , 2019
-                                        <br />
-                                        Como ya empieza a ser tradición, este año hemos vuelto a participar en la reunión de Akafliegs organizada en Alemania todos los veranos, ¡esta vez como
-                                    </Card.Text>
-                                    <Link to={`/news/`}>
-                                        <Button >Leer más</Button>
-                                    </Link>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col >
-                            <Card className="newsCard">
-                                <Card.Img src={news3} />
-                                <Card.Body className="">
-                                    <Card.Title className="center blue">Impresora 3D</Card.Title>
-                                    <Card.Text>
-                                        13 Mar , 2019
-                                        <br />
-                                        A medida que avanzábamos en los proyectos, se hacia más y más evidente la necesidad de poder fabricar distintos moldes y piezas para los proyectos, por lo que</Card.Text>
-                                    <Link to={`/news/`}>
-                                        <Button>Leer más</Button>
-                                    </Link>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-
-                    </Row>
-                </section>
-                <section className="prices">
-
-                    <div className="prices-title">
-                        <Row>
-                            <h2 className="center ">Nuestros precios</h2>
-
-                            <p className="center ">Cuando salimos a volar, ser miembro de nuestro Akaflieg tiene muchas ventajas.</p>
+                            </Col>
                         </Row>
-                    </div>
-                    <Row>
-                        <Col>
-                            <div class="a-box ">
-                                <div class="img-container ">
-                                    <div class="img-inner">
-                                        <div class="inner-skew">
-                                            <img src={k7plane} />
+
+                    </section>
+                </div>
+
+                <Container >
+                    <section className="newsSection">
+                        <Row>
+                            <Col className="newsTitle ">Noticias</Col>
+                        </Row>
+                        <hr />
+                        <Row>
+                            <Col >
+                                <Card className="newsCard">
+                                    <Card.Img src={news1} />
+                                    <Card.Body >
+                                        <Card.Title className="center blue">Sommertreffen 2019</Card.Title>
+                                        <Card.Text >
+                                            12 Sep , 2019
+                                            <br />
+                                            Desde el Club de Vuelo, entre muchas otras cosas, buscamos fomentar la cultura aeronáutica a través nuestra participación y organización de actividades</Card.Text>
+                                        <Link to={`/news`}>
+                                            <Button>Leer más</Button>
+                                        </Link>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col >
+                                <Card className="newsCard">
+                                    <Card.Img src={news2} />
+                                    <Card.Body className="">
+                                        <Card.Title className="center blue">Cursillo ATC IVAO</Card.Title>
+                                        <Card.Text>
+                                            16 Mar , 2019
+                                            <br />
+                                            Como ya empieza a ser tradición, este año hemos vuelto a participar en la reunión de Akafliegs organizada en Alemania todos los veranos, ¡esta vez como
+                                        </Card.Text>
+                                        <Link to={`/news`}>
+                                            <Button >Leer más</Button>
+                                        </Link>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                            <Col >
+                                <Card className="newsCard">
+                                    <Card.Img src={news3} />
+                                    <Card.Body className="">
+                                        <Card.Title className="center blue">Impresora 3D</Card.Title>
+                                        <Card.Text>
+                                            13 Mar , 2019
+                                            <br />
+                                            A medida que avanzábamos en los proyectos, se hacia más y más evidente la necesidad de poder fabricar distintos moldes y piezas para los proyectos, por lo que</Card.Text>
+                                        <Link to={`/news`}>
+                                            <Button>Leer más</Button>
+                                        </Link>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+
+                        </Row>
+                    </section>
+                    <section className="prices">
+
+                        <div className="prices-title">
+                            <Row>
+                                <h2 className="center ">Nuestros precios</h2>
+                                <hr />
+
+                                <p className="center ">Cuando salimos a volar, ser miembro de nuestro Akaflieg tiene muchas ventajas.</p>
+                            </Row>
+                        </div>
+                        <Row>
+                            <Col>
+                                <div class="a-box ">
+                                    <div class="img-container ">
+                                        <div class="img-inner">
+                                            <div class="inner-skew">
+                                                <img src={k7plane} />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="text-container">
+                                        <h3>Vuelo en K7</h3>
+                                        <div>
+                                            El primer avión del club fue nuestro planeador "old-school", Alexander Schleicher K7 Rhönadler. Tiene espacio para dos personas, así que si quieres volar acompañado solo te costará 0.20€/min.
                                         </div>
                                     </div>
                                 </div>
-                                <div class="text-container">
-                                    <h3>Vuelo en K7</h3>
-                                    <div>
-                                        El primer avión del club fue nuestro planeador "old-school", Alexander Schleicher K7 Rhönadler. Tiene espacio para dos personas, así que si quieres volar acompañado solo te costará 0.20€/min.
-                                    </div>
-                                </div>
-                            </div>
-                        </Col>
-                        <Col>
+                            </Col>
+                            <Col>
 
-                            <div class="a-box">
-                                <div class="img-container">
-                                    <div class="img-inner">
-                                        <div class="inner-skew">
-                                            <img src={SB5} />
+                                <div class="a-box">
+                                    <div class="img-container">
+                                        <div class="img-inner">
+                                            <div class="inner-skew">
+                                                <img src={SB5} />
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="text-container">
-                                    <h3>Vuelo en SB-5</h3>
-                                    <div>
-                                        Si por el contrario prefieres volar solo en nuestro avión monoplaza de la Akaflieg Braunschweig, SB-5 Danzig, te costará <strong>0.30€/min</strong>.                                    </div>
-                                </div>
-                            </div>
-                        </Col>
-
-                        <Col>
-
-                            <div class="a-box">
-                                <div class="img-container">
-                                    <div class="img-inner">
-                                        <div class="inner-skew">
-                                            <img src={pawnee} />
-                                        </div>
+                                    <div class="text-container">
+                                        <h3>Vuelo en SB-5</h3>
+                                        <div>
+                                            Si por el contrario prefieres volar solo en nuestro avión monoplaza de la Akaflieg Braunschweig, SB-5 Danzig, te costará <strong>0.30€/min</strong>.                                    </div>
                                     </div>
                                 </div>
-                                <div class="text-container">
-                                    <h3>Remolque en nuestra Pawnee</h3>
-                                    <div>
-                                        Llegado a este punto te preguntarás cómo vuelan estos aviones si no tienen motor... Para ello necesitamos que otro avión nos remolque y todo esto cuesta 36€ más.                                    </div>
+                            </Col>
+
+                            <Col>
+
+                                <div class="a-box">
+                                    <div class="img-container">
+                                        <div class="img-inner">
+                                            <div class="inner-skew">
+                                                <img src={pawnee} />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="text-container">
+                                        <h3>Remolque en nuestra Pawnee</h3>
+                                        <div>
+                                            Llegado a este punto te preguntarás cómo vuelan estos aviones si no tienen motor... Para ello necesitamos que otro avión nos remolque y todo esto cuesta 36€ más.                                    </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </Col>
+                            </Col>
 
 
-                    </Row>
-                </section>
+                        </Row>
+                    </section>
 
-                <section>
-                    <Row><h2 className="center mb">Redes Sociales</h2></Row>
-                    <Row>
-                        <Col className="facebook"><Facebook></Facebook></Col>
+                    <section>
+                        <Row><h2 className="center mb box">Redes Sociales</h2></Row>
+                        <Row>
+                            <Col className="facebook"><Facebook></Facebook></Col>
 
-                        <Col className="twitter"><Twitter></Twitter></Col>
-                    </Row>
-                </section>
+                            <Col className="twitter"><Twitter></Twitter></Col>
+                        </Row>
+                    </section>
 
-            </Container>
+                </Container>
+            </div>
         </>
     )
 }
